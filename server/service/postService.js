729a6992d8175e2d7aa2fff0ea2a14d20d263a3c
Edson Luiz Hervis Junior <juniorhervis1@gmail.com>
2022-02-24 15:16:47 -0300

    implementações no código

--- server/service/postService.js
+++ server/service/postService.js
@@ -4,12 +4,18 @@ exports.getPosts = function () {
   return postsData.getPosts();
 };
 
-
-exports.savePost = function (post) {
-    return postsData.savePost(post)
-
+exports.getPost = function (id) {
+  return postsData.getPost(id);
 };
 
-exports.deletePost = function(id) {
-  return postsData.deletePost(id)
-}
+exports.savePost = function (post) {
+  return postsData.savePost(post);
+};
+
+exports.deletePost = function (id) {
+  return postsData.deletePost(id);
+};
+
+exports.updatePost = function (id, post) {
+  return postsData.updatePost(id, post);
+};
--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -6,8 +6,15 @@ const generate = function () {
   return crypto.randomBytes(20).toString("hex");
 };
 
+const request = function (url, method, data) {
+  return axios({
+    url,
+    method,
+    data,
+  });
+};
+
 test("Should get posts", async function () {
-  //givem - dado que
   const post1 = await postService.savePost({
     title: generate(),
     content: generate(),
@@ -20,15 +27,46 @@ test("Should get posts", async function () {
     title: generate(),
     content: generate(),
   });
-  // when - quando
-  const response = await axios({
-    url: "http://localhost:3000/posts",
-    method: "get",
-  });
+  const response = await request("http://localhost:3000/posts", "get");
   const posts = response.data;
-  // then - então
   expect(posts).toHaveLength(3);
   await postService.deletePost(post1.id);
   await postService.deletePost(post2.id);
   await postService.deletePost(post3.id);
+});
+
+
+test("Should save posts", async function () {
+  const data = {
+    title: generate(), content: generate()};
+
+  const response = await request("http://localhost:3000/posts", "post", data);
+  const posts = response.data;
+  expect(posts.title).toBe(data.title)
+  expect(posts.content).toBe(data.content)
+  await postService.deletePost(posts.id);
+});
+
+test("Should update a post", async function () {
+  const post = await postService.savePost({
+    title: generate(),
+    content: generate(),
+  });
+  post.title = generate()
+  post.content = generate()
+  await request(`http://localhost:3000/posts/${post.id}`, "put", post);
+  const updatePost = await postService.getPost(post.id)
+  expect(updatePost.title).toBe(post.title)
+  expect(updatePost.content).toBe(post.content)
+  await postService.deletePost(post.id);
+});
+
+test("Should delete post", async function () {
+  const post = await postService.savePost({
+    title: generate(),
+    content: generate(),
+  });
+  await request(`http://localhost:3000/posts/${post.id}`, "delete");
+  const posts = await postService.getPosts()
+  expect(posts).toHaveLength(0)
 });
