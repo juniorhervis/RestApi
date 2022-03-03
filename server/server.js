6134f1b1889094f64b3bce7382a11e82d620dff1
Edson Luiz Hervis Junior <juniorhervis1@gmail.com>
2022-03-03 15:00:46 -0300

    confirmar

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use("/", require("./route/postsRoute"));
+app.use(function (error, req, res, next) {
+  if (error.message === "Post already exists!") {
+    return res.status(409).send(e.message);
+  }
+  if (error.message === "Post not found!") {
+    return res.status(404).send(e.message);
+  }
+  res.status(500).send(e.message);
+});
 
 app.listen(3000, () => {
-    console.log("API Started!")
+  console.log("API Started!");
 });
